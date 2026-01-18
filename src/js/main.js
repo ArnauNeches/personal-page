ea7303f3d256fdf031eebb22fbbd90243c2a9c56
ArnauNeches <arnichi2103@gmail.com>
2026-01-18 18:03:57 +0100

    added footer renderer

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 import { renderBooks } from "./books.js";
 import { renderProjects } from "./projects.js";
+import { renderFooter } from "./components/renderFooter.js";
+
+const footerContainer = document.getElementById("footer");
+if (footerContainer) {
+    footerContainer.appendChild(renderFooter());
+}
 
 if (document.getElementById("projects")) {
     renderProjects();
