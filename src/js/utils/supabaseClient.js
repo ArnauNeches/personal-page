bd1e89d36f7e758dafbca2c971163e19c914702b
ArnauNeches <arnichi2103@gmail.com>
2026-07-12 10:30:33 +0200

    ops

--- src/js/utils/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-
-const SUPABASE_URL = 'https://ofjvjcscbgjwbpptxzsf.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_ZlfYLSyN7dFNhmSU2sEkBA__IOnV23B';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
--- versions/v3/src/js/utils/deleteSuggestion.js
+++
@@ -1,9 +0,0 @@
-export function deleteSuggestion(container) {
-
-    const id = container.dataset.id;
-    container.remove();
-    const suggestions = JSON.parse(localStorage.getItem("suggestions")) || [];
-    const updated = suggestions.filter(s => s.id != id);
-    localStorage.setItem("suggestions", JSON.stringify(updated));
-    
-}
